bbbd377f8cfb3e7656b051b8cd568797f6e05fc0
Sam Buters <sambuters@gmail.com>
2020-09-02 13:24:07 -0300

    Adding dummy target and enemy follow function

--- src/enemy.js
+++ src/enemy.js
@@ -3,5 +3,15 @@ class Enemy {
 		this.entity = game.physics.add.sprite(x, y, 'spr_enemy');
 		this.entity.setCollideWorldBounds(true);
 		this.target = player;
+		this.game = game;
+	}
+	
+	followTarget() {
+		this.entity.rotation = Phaser.Math.Angle.Between(this.entity.x, this.entity.y, this.target.x, this.target.y);
+		this.game.physics.velocityFromRotation(this.entity.rotation, 200, this.entity.body.velocity);
+	}
+	
+	update() {
+		followTarget();
 	}
 }
--- src/game.js
+++ src/game.js
@@ -17,13 +17,25 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var test_enemy;
+var dummy_target;
 
 function preload() {
 	this.load.spritesheet('spr_enemy', 'assets/spr_enemy.png', { frameWidth: 32, frameHeight: 32 });
+	this.load.image('spr_target', 'assets/spr_target.png');
 }
 
 function create() {
-	var test_enemy = new Enemy(this, 200, 300);	
+	dummy_target = this.add.image(this.input.activePointer.x, this.input.activePointer.y, 'spr_target');
+	test_enemy = new Enemy(this, 200, 300, dummy_target);
+	
+	this.input.on('pointermove', (e) => {
+		dummy_target.x = e.position.x;
+		dummy_target.y = e.position.y;
+	});
 }
 
-function update() {}
+function update() {
+	//console.log(game.physics)
+	test_enemy.update();
+}
