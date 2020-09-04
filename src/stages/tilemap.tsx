60d6a74238b72618b78f42cc8acd7b2b5486eec6
Sam Buters <sambuters@gmail.com>
2020-09-04 20:59:41 -0300

    Adding properties to stage, stage info files and stage 1

---
+++ src/stages/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tilemap" tilewidth="16" tileheight="16" tilecount="35" columns="7">
+ <image source="../../assets/tilemap.png" width="112" height="80"/>
+</tileset>
