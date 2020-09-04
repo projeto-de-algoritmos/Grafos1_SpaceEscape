ad6662f46dd626ec83eeabdd13648d780f1f5192
Sam Buters <sambuters@gmail.com>
2020-09-04 00:06:25 -0300

    Adding graph class

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,19 @@ function cast_ray_into_tilemap(x0, y0, x1, y1, layer) {
 	ray = new Phaser.Geom.Line(x0, y0, x1, y1);
 	return layer.getTilesWithinShape(ray, {isNotEmpty: true})
 }
+
+class Graph {
+	constructor() {
+		this.vertices = 0;
+		this.adjList = new Map();
+	}
+	
+	addVertex(v) {
+		this.adjList.set(v, []);
+	}
+	
+	addEdge(v1, v2) {
+		this.adjList.get(v1).push(v2);
+		this.adjList.get(v2).push(v1);
+	}
+}
